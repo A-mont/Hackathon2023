aacaae4f9d829ba32b7a5f5070f508680a5ed75e
A Mon <a.mont.24.777@gmail.com>
2023-10-23 14:49:03 -0600

    Update

---
+++ Tutorial-03/Scrow_contract/state/src/lib.rs
@@ -0,0 +1,27 @@
+
+#![no_std]
+
+use io::*;
+use gmeta::{ Metadata, metawasm};
+use gstd::{ ActorId, prelude::*};
+
+
+#[cfg(feature = "binary-vendor")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+
+#[metawasm]
+pub mod metafns {
+    pub type State = Escrow;
+
+    pub fn seller(state: State) -> ActorId {
+        state.seller
+    }
+
+    pub fn buyer(state: State) -> ActorId {
+        state.buyer
+    }
+
+    pub fn escrow_state(state: State) -> EscrowState {
+        state.state
+    }
+}
