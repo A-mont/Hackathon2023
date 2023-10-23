a5be582ad1b69470f4c1dafff7645b969835b32d
A Mon <a.mont.24.777@gmail.com>
2023-10-23 14:48:25 -0600

    Update

---
+++ Tutorial-03/Scrow_contract/io/src/lib.rs
@@ -0,0 +1,54 @@
+
+#![no_std]
+use gstd::{ prelude::*, ActorId };
+use gmeta::{InOut,Metadata};
+
+
+#[derive(Decode,Encode, TypeInfo)]
+pub struct InitEscrow {
+    pub seller: ActorId,
+    pub buyer: ActorId,
+    pub price: u128,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum EscrowAction {
+    Deposit,
+    ConfirmDelivery,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum EscrowEvent {
+    FundsDeposited,
+    DeliveryConfirmed,
+}
+
+
+
+#[derive(Default, Encode, Decode, TypeInfo)]
+pub struct Escrow {
+    pub seller: ActorId,
+    pub buyer: ActorId,
+    pub price: u128,
+    pub state: EscrowState,
+}
+
+#[derive(Default, Debug, PartialEq, Eq, Encode, Decode, TypeInfo)]
+pub enum EscrowState {
+    #[default]
+    AwaitingPayment,
+    AwaitingDelivery,
+    Closed,
+}
+
+pub struct ContractMetadata;
+
+
+impl Metadata for ProgramMetadata {
+    type Init = In<InitEscrow>; // Definimos el struct que queremos de inicio.
+    type Handle = InOut<EscrowAction, EscrowEvent>;
+    type Reply = ();
+    type Others = ();
+    type Signal = ();
+    type State = Escrow;
+}
